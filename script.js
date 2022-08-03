dffe8c9c28514780b0762f26a7be9972262d9b81
Diana <dianochka.muzychenko@gmail.com>
2022-08-03 11:36:51 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+//const category = 'toys';
+
+//console.log(`https://someurl.com/${category}`);
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+console.log(4 * 2 == 8);
