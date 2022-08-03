855d5638167c29b8acd0aea0c21c7cec760062ce
Diana <dianochka.muzychenko@gmail.com>
2022-08-03 12:40:57 +0300

    starting app

---
+++ app.js
@@ -0,0 +1,19 @@
+const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+
+const personalMovieDB = {
+      count: numberOfFilms,
+      movies: {},
+      actors: {},
+      genres: [],
+      privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?'),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
