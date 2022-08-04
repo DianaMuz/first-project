c4ffdfcc9f42c3f31d831f5924b77b14e29d516c
Diana <dianochka.muzychenko@gmail.com>
2022-08-04 12:28:17 +0300

    updated with cycles

--- app.js
+++ app.js
@@ -8,12 +8,32 @@ const personalMovieDB = {
       privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?'),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done!');
+    } else {
+        console.log('Error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} 
+else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+}
+else if (personalMovieDB >= 30) {
+    console.log('Вы киноман!');
+}
+else {
+    console.log('Произошла ошибка');
+}
+
 
 console.log(personalMovieDB);
--- script.js
+++ script.js
@@ -14,3 +14,18 @@ console.log(incr);
 console.log(decr);
 
 console.log(4 * 2 == 8);
+
+let num = 22;
+
+while (num <= 29) {
+    console.log(num);
+    num++;
+}
+
+let numb = 21;
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 25);
